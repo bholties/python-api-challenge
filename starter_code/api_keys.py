7dbf9ca81abd9767fcee5bedfc95c3586cdadc14
bholties <77474724+bholties@users.noreply.github.com>
2021-07-15 17:30:46 -0500

    adding api keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8ea89b1be9d40192c4aacd70528b992f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC_Nxybp0HYbjdPfW3Dgrhqpw5RYNqyJlY"
